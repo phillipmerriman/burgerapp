20bf1e6540c9c832148dfe667d224cd96b66bd64
Phillip Merriman <phillipmerriman@gmail.com>
2021-01-10 14:21:38 -0600

    fully functional. just updating visuals

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('public/assets/images'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
